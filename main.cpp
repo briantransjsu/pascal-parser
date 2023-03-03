fa9a06c40919a96fadfcedf92fdf5b6bc4355abe
Your Name <you@example.com>
2023-03-02 17:36:35 -0800

    Simple scanner added only for integer statements

--- main.cpp
+++ main.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
+
+#include "parser.h"
 #include "symtab.h"
 using std::cout, std::endl;
+#include "scanner.h"
 
 int main(int argc, char *argv[]) {
-      SymbolTable symTable;
-
-    // Add some symbols to the table
-    symTable.add_symbol("x", "integer", 10);
-    symTable.add_symbol("y", "integer", 10);
-
-    // Check if a symbol exists and retrieve its information
-    if (symTable.symbol_exists("x")) {
-      //unwrap the x value
-      if (auto symbol = symTable.get_symbol("x")) {
-        cout << "Symbol name: " << symbol -> name << endl;
-        cout << "Symbol type: " << symbol -> type << endl;
-        cout << "Symbol value: " << symbol -> value << endl;
-      }
-      //unwrap the y value
-      if (auto symbol = symTable.get_symbol("y")) {
-        cout << "Symbol name: " << symbol -> name << endl;
-        cout << "Symbol type: " << symbol -> type << endl;
-        cout << "Symbol value: " << symbol -> value << endl;
-      }
-      //unknown
-      if (auto symbol = symTable.get_symbol("z")) {
-        cout << "Symbol name: " << symbol -> name << endl;
-        cout << "Symbol type: " << symbol -> type << endl;
-        cout << "Symbol value: " << symbol -> value << endl;
-      } else {
-	std::cout << "No \"z\" found\n";
+  std::string input = "x := 5; y := 10;";
+  int position = 0;
+  cmpe125_scanner::Token token = cmpe125_scanner::getNextToken(input, position);
+  while (token.type != cmpe125_scanner::END_OF_FILE) {
+    if (token.type == cmpe125_scanner::IDENTIFIER &&
+        cmpe125_scanner::getNextToken(input, position).value == ":=") {
+      cmpe125_scanner::Token nextToken =
+          cmpe125_scanner::getNextToken(input, position);
+      if (nextToken.type == cmpe125_scanner::NUMBER) {
+        cout << "Assigning " << nextToken.value << " to variable "
+             << token.value << endl;
       }
     }
-    symTable.outputSymbolTable();
-    return 0;
+    token = cmpe125_scanner::getNextToken(input, position);
+  }
 }
